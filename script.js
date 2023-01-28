fc55633352979b5c3ad4d48c715b5f1666d0281a
yahyaeslem <yahyaeslem@gmail.com>
2023-01-28 23:41:09 +0300

    created table

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const library=document.querySelector('#library')
  const form = document.querySelector('form');
+ const tableBody =document.querySelector('.tableBody')
  form.addEventListener('submit', handleSubmit);
 const btnNewBook = document.querySelector(".btnNewBook")
 const submitButton = document.querySelector(".submit")
@@ -23,33 +24,46 @@ function addBookToLibrary(value){
    newBook = new Book (value.title, value.author, value.number, value.read)
     myLibrary.push(newBook);
     var datacount= myLibrary.length- 1;
-        books = document.createElement('div');
-        library.appendChild(books);
+        books = document.createElement('tr');
+        tableBody.appendChild(books);
         books.classList.add('books');
         books.setAttribute('id', datacount)
     
-       let titlediv = document.createElement('div');
+        let titlediv = document.createElement('td');
         books.appendChild(titlediv)
         titlediv.classList.add('bookSpec')
         
-        let authordiv= document.createElement('div');
+        let authordiv= document.createElement('td');
         books.appendChild(authordiv)
         authordiv.classList.add('bookSpec')
-        let numberdiv= document.createElement('div');
+        let numberdiv= document.createElement('td');
         books.appendChild(numberdiv)
         numberdiv.classList.add('bookSpec')
-       let readdiv= document.createElement('button');
+        let readdiv = document.createElement('td')
         books.appendChild(readdiv)
         readdiv.classList.add('bookSpec')
-        let remove = document.createElement('button');
-        books.appendChild(remove);
-        remove.classList.add('removeBtn')
-        remove.setAttribute('id', datacount)
-        titlediv.innerHTML = "Title" + ""+ ":"+""+ value.title;
-        authordiv.innerHTML = "Author" + ""+ ":"+ ""+value.author;
-        numberdiv.innerHTML = "Number" + ""+ ":"+ ""+value.number;
-        readdiv.innerHTML = "Read " + ""+ ":"+ ""+value.read;
-      
+        let readLabel = document.createElement('label')
+        let readSelect = document.createElement('select');
+        let readOptionYes = document.createElement('option');
+        let readOptionNo = document.createElement('option');
+        readdiv.appendChild(readLabel)
+        readLabel.appendChild(readSelect)
+        readSelect.appendChild(readOptionYes)
+        readSelect.appendChild(readOptionNo)
+        readOptionYes.setAttribute("value","yes") 
+        readOptionYes.innerHTML= "Yes"
+        readOptionNo.setAttribute("value","no")
+        readOptionNo.innerHTML="No"
+        // let remove = document.createElement('button');
+        // books.appendChild(remove);
+        // remove.classList.add('removeBtn')
+        // remove.setAttribute('id', datacount)
+        readLabel.setAttribute("for", "readform")
+
+        titlediv.innerHTML = value.title;
+        authordiv.innerHTML = value.author;
+        numberdiv.innerHTML = value.number;
+
     
 }
 var removeID;
